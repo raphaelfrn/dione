94d171871f0f8451a7b6a4d6cd0cec5f95f66764
Raphaël <raphfar@gmail.com>
2022-10-03 15:15:36 +0200

    init

---
+++ src/main/java/com/dione/DioneApplication.java
@@ -0,0 +1,13 @@
+package com.dione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dione/DioneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
